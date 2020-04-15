8cdb3a0b36d5b9c005617283324344f365c539b7
Abdul eriawan nahar <lorpesarya@gmail.com>
2020-04-15 11:19:22 +0700

    create dbhelper and activity display

--- app/src/main/java/com/example/exersice_sqlite/DBHelper.java
+++ app/src/main/java/com/example/exersice_sqlite/DBHelper.java
@@ -11,5 +11,71 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "MyDBName.db";
+    public static final String MHS_TABLE_NAME = "Data";
+    public static final String MHS_COLUMN_ID = "id";
+    public static final String MHS_COLUMN_EMAIL = "email";
+    public static final String MHS_COLUMN_NAMA = "nama";
+    public static final String MHS_COLUMN_PHONE = "telepon";
+    public static final String MHS_COLUMN_ALAMAT = "alamat";
+    private HashMap hp;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table Data " +
+                        "(id integer primary key, nama text,alamat text,phone text, email text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int
+            newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS Data");
+        onCreate(db);
+    }
+    public boolean insertContact (String nama, String alamat, String phone, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("nama", nama);
+        contentValues.put("alamat", alamat);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        db.insert("Data", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery( "select * from data where id="+id+"", null );
+        return res;
+    }
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db,
+                MHS_TABLE_NAME);
+        return numRows;
+    }
+
+    public ArrayList<String> getAllCotacts() {
+        ArrayList<String> array_list = new ArrayList<String>();
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery( "select * from data", null );
+        res.moveToFirst();
+        while(res.isAfterLast() == false){
+
+            array_list.add(res.getString(res.getColumnIndex(MHS_COLUMN_NAMA)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
 }
+
+
