8c4497df4c761190d57d614fd7618f88978e26eb
Abdul eriawan nahar <lorpesarya@gmail.com>
2020-04-15 14:31:30 +0700

    Add Display data UI

--- app/src/main/java/com/example/exersice_sqlite/MainActivity.java
+++ app/src/main/java/com/example/exersice_sqlite/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.exersice_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab=(FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==fab){
+            Intent intent = new Intent(this, DisplayData.class);
+            startActivity(intent);
+        }
     }
 }
